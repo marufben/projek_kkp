95debd478ff5d29d1cf3771de00c7299ec5f2941
hrydi <hari.nube@gmail.com>
2015-07-14 14:46:46 +0700

    update statusizin views

--- application/views/statusizin/input.php
+++ application/views/statusizin/input.php
@@ -15,10 +15,33 @@
 	        </div>
 	        <div class="panel-body">
 				<div class="form-group">
-					<label for="inputEmail5" class="col-sm-2 control-label">Nama</label>
+					<label for="inputEmail5" class="col-sm-2 control-label">Jenis Izin</label>
 					<div class="col-sm-4">
 						<input type="hidden" name='id' value="<?php echo $status->id; ?>">
-						<input type="text" class="form-control" id="status_ijin" name='status_ijin' value="<?php echo $status->status_ijin; ?>" placeholder="nama status">
+						<select class="form-control" id="id_jenis" name='id_jenis'>
+							<option value="0">--Pilih--</option>
+							<?php
+							foreach ($jenis as $key => $value) {
+							$select = ($status->id_jenis == $value->id)?"selected":"";
+							?>
+							<option <?php echo $select;?> value="<?php echo $value->id;?>"><?php echo $value->singkatan;?></option>
+							<?php
+							}
+							?>
+						</select>
+
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="inputEmail5" class="col-sm-2 control-label">Kode No</label>
+					<div class="col-sm-4">
+						<input type="text" readonly class="form-control" id="kode_no" name='kode_no' value="<?php echo $status->kode_no; ?>" placeholder="kode no">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="inputEmail5" class="col-sm-2 control-label">Nama</label>
+					<div class="col-sm-4">
+						<input type="text" class="form-control" id="nama" name='nama' value="<?php echo $status->nama; ?>" placeholder="nama status">
 					</div>
 				</div>
 				<div class="form-group">
@@ -41,9 +64,21 @@
 </div>
 
 <script type="text/javascript">
-(function(){
+$('#id_jenis').on('change', function(){
+	var val = $(this);
+	cek(val.val())
+})
 
-})();
-
-
+var cek = function(val){
+	$.ajax({
+		url: "<?php echo base_url();?>statusizin/cek",
+		type: 'post',
+		data: {
+			id_jenis: val
+		},
+		success: function(res){
+			$('#kode_no').val(res)
+		}
+	})
+}
 </script>
