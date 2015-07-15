85ee41697f2db99ff34e9afdf3cf72f195d20bbf
marufben <marufben@gmail.com>
2015-07-15 09:32:40 +0700

    new model arsip

--- application/models/arsip_model.php
+++ application/models/arsip_model.php
@@ -15,20 +15,23 @@ class arsip_model extends CI_Model {
 	}
 	// public function get_table
 	public function insert_arsip(){
+		$kapal=explode('|',$this->input->post('kapal'));
+		$jenis_ijin=explode('|',$this->input->post('jenis_ijin'));
 		$data=array(
 			'judul'=>$this->input->post('judul'),
 			'id_perusahaan'=>$this->input->post('perusahaan'),
-			'id_kapal'=>$this->input->post('kapal'),
+			'id_kapal'=>$kapal[0],
 			'kode_barcode'=>$this->input->post('barcode'),
 			'status_ijin'=>$this->input->post('status_ijin'),
-			'jenis_ijin'=>$this->input->post('jenis_ijin'),
-			'no_izin'=>$this->input->post('no_ijin'),
+			'jenis_ijin'=>$jenis_ijin[0],
+			'no_ijin'=>$this->input->post('no_ijin'),
 			'tgl_terbit'=>$this->input->post('terbit'),
 			'tgl_expired'=>$this->input->post('expired'),
 			// 'jumlah_halaman'=>$this->input->post(''),
 			'no_pembukuan'=>$this->input->post('no_pembukuan'),
-			'tgl_pembukuan'=>$this->input->post('tgl_pembukuan'),
+			'tgl_pembukuan'=>date('Y-m-d'),
 		);
+
 		$this->db->insert('arsip', $data);
 	}
 	// public function upload_lampiran(){
